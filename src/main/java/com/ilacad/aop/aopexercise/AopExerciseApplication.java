b530618673ea64a2aa494c3f18ef118b35f292e9
jcilacad <johnchristopherilacad27@gmail.com>
2023-09-06 20:04:15 +0800

    Setup project

---
+++ src/main/java/com/ilacad/aop/aopexercise/AopExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.ilacad.aop.aopexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopExerciseApplication.class, args);
+	}
+
+}
