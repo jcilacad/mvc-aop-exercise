f16f8a2b46b3085058fc623ee11f9684080d25aa
jcilacad <johnchristopherilacad27@gmail.com>
2023-09-07 18:33:55 +0800

    Add a model for userDto

--- src/main/java/com/ilacad/aop/aopexercise/controller/UserController.java
+++ src/main/java/com/ilacad/aop/aopexercise/controller/UserController.java
@@ -1,13 +1,18 @@
 package com.ilacad.aop.aopexercise.controller;
 
+import com.ilacad.aop.aopexercise.dto.UserDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
     @GetMapping("/add-user")
-    public String addUser() {
+    public String addUser(Model model) {
+
+        model.addAttribute("userDto", new UserDto());
+        
         return "add-user";
     }
 }
