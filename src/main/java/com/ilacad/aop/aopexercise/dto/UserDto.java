990d930196932ce2d98dd72651ee9734d9e10267
jcilacad <johnchristopherilacad27@gmail.com>
2023-09-07 18:31:49 +0800

    Create a UserDto

---
+++ src/main/java/com/ilacad/aop/aopexercise/dto/UserDto.java
@@ -0,0 +1,19 @@
+package com.ilacad.aop.aopexercise.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserDto {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+}
