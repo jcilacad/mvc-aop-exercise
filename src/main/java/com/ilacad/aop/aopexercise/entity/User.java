e01ff6742973ff6ace8a4255677c80e8e8c79cb3
jcilacad <johnchristopherilacad27@gmail.com>
2023-09-07 18:54:32 +0800

    Create a logic to add user in database

--- src/main/java/com/ilacad/aop/aopexercise/entity/User.java
+++ src/main/java/com/ilacad/aop/aopexercise/entity/User.java
@@ -28,4 +28,9 @@ public class User {
     @Column(name = "email", unique = true, nullable = false)
     private String email;
 
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
 }
--- src/main/java/com/ilacad/aop/aopexercise/service/UserServiceImpl.java
+++ src/main/java/com/ilacad/aop/aopexercise/service/UserServiceImpl.java
@@ -1,13 +1,32 @@
 package com.ilacad.aop.aopexercise.service;
 
 import com.ilacad.aop.aopexercise.dto.UserDto;
+import com.ilacad.aop.aopexercise.entity.User;
+import com.ilacad.aop.aopexercise.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public void addUser(UserDto userDto) {
+
+        // Getting the data from UserDto
+        String firstName = userDto.getFirstName();
+        String lastName = userDto.getLastName();
+        String email = userDto.getEmail();
+
+        // Create an instance of User and Save in Database
+        User user = new User(firstName, lastName, email);
+        userRepository.save(user);
 
     }
 }
