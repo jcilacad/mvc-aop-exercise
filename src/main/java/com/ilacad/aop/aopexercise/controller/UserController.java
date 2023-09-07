cfe17bfcf7be342399e120fe19ff8e56b74b501a
jcilacad <johnchristopherilacad27@gmail.com>
2023-09-07 18:40:54 +0800

    Add a post mapping for add-user

--- src/main/java/com/ilacad/aop/aopexercise/controller/UserController.java
+++ src/main/java/com/ilacad/aop/aopexercise/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.ilacad.aop.aopexercise.controller;
 
 import com.ilacad.aop.aopexercise.dto.UserDto;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -15,4 +18,11 @@ public class UserController {
 
         return "user";
     }
+
+    @PostMapping("/add-user")
+    public String addUser(@Valid @ModelAttribute(name = "userDto") UserDto userDto) {
+
+        return "redirect:/add-user?success";
+    }
+
 }
