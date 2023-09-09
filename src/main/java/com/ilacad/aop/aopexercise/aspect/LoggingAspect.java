d79ffcec7ffa70bfcc91e48d07e0df7bc3e1655c
jcilacad <johnchristopherilacad27@gmail.com>
2023-09-09 18:42:34 +0800

    Create a pointcut decalaration for service package and apply it on the the before advice

--- src/main/java/com/ilacad/aop/aopexercise/aspect/LoggingAspect.java
+++ src/main/java/com/ilacad/aop/aopexercise/aspect/LoggingAspect.java
@@ -2,14 +2,17 @@ package com.ilacad.aop.aopexercise.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect {
 
+    @Pointcut("execution(* com.ilacad.aop.aopexercise.service.*.*(..))")
+    private void forServicePackage() {}
 
-    @Before("execution(* com.ilacad.aop.aopexercise.service.*.*(..))")
+    @Before("forServicePackage()")
     public void beforeAddAccountAdvice() {
 
         System.out.println("Message Before Adding User");
