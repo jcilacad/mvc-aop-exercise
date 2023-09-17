fcd7f76516a28aa684bb7ce6fa51a92ebd938619
jcilacad <johnchristopherilacad27@gmail.com>
2023-09-18 01:10:30 +0800

    Separate 2 aspects to give an order with the same pointcut execution.

---
+++ src/main/java/com/ilacad/aop/aopexercise/aspect/MyApiAnalyticsAspect.java
@@ -0,0 +1,9 @@
+package com.ilacad.aop.aopexercise.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyApiAnalyticsAspect {
+}
