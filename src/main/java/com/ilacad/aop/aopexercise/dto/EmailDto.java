3e258f59bd5a9de7cebf60d0394c92d3124846b8
jcilacad <johnchristopherilacad27@gmail.com>
2023-09-23 01:25:42 +0800

    Create an EmailDto

---
+++ src/main/java/com/ilacad/aop/aopexercise/dto/EmailDto.java
@@ -0,0 +1,18 @@
+package com.ilacad.aop.aopexercise.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class EmailDto {
+
+    @NotEmpty(message = "Email Must Not Be Empty")
+    private String email;
+    
+}
